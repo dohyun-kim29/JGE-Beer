ffc219a9e03460c17fd0ab6026a4366e16f6bf65
DohyunKIm <rlaehgus1229@gmail.com>
2021-04-19 15:04:39 +0900

    [Refactor] RandomViewModel

--- JGE-Beer/ViewModels/RandomViewModel.swift
+++ JGE-Beer/ViewModels/RandomViewModel.swift
@@ -10,39 +10,40 @@ import RxCocoa
 import Moya
 
 class RandomViewModel: ViewModelType {
-    private var disposeBag = DisposeBag()
-    
+   
     // MARK: - ViewModelType
+    // 김정빈 다녀갑니다
     
     struct Input {
-        let provider: MoyaProvider<BeerAPI>
         let buttonTrigger: Signal<Void>
     }
     
     struct Output {
-        let beer: BehaviorRelay<[Beer]>
+        let beer: Signal<[Beer]>
         let isLoading: Signal<Bool>
-        let errorRelay: PublishRelay<Error>
+        let errorRelay: PublishRelay<String>
     }
     
-    func transform(input: Input) -> Output {
+    // 현욱이 코드 상당히 많이 참조.. 넘 어려워요..
+    init() {
         let activityIndicator = ActivityIndicator()
-        let beer = BehaviorRelay<[Beer]>(value: [])
-        let errorRelay = PublishRelay<Error>()
+        let beer: PublishRelay<String> = PublishRelay()
         
-        input.buttonTrigger
-            .asObservable()
-            .flatMapLatest {
-                input.provider.rx.request(.random)
-                    .filterSuccessfulStatusCodes()
-                    .map([Beer].self)
-                    .trackActivity(activityIndicator)
-                    .do(onError: { errorRelay.accept($0) })
-                    .catchErrorJustReturn([])
-            }
-            .bind(to: beer)
-            .disposed(by: disposeBag)
+        input = Input()
+        output = Output(beer: input.buttonTrigger.asObservable().catchError { err -> Observable<String> in
+            beer.accept(err.localizedDescription)
+            return Observable<String>.just("")
+        }.flatMapLatest {
+            provider.rx.request(.random)
+                .filterSuccessfulStatusCodes()
+                .map([Beer.self])
+            
+        }.asSignal(onErrorJustReturn: []),
+        isLoading:
+            activityIndicator.asSignal(onErrorJustReturn: false), errorRelay: errorRelay
+        )
         
-        return Output(beer: beer, isLoading: activityIndicator.asSignal(onErrorJustReturn: false), errorRelay: errorRelay)
     }
+    
+    
 }
